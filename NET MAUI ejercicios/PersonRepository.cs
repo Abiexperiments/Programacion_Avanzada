e8fedd8ed5fced0c402596133bde8aa7beb5f4e5
Abiexperiments <abigailgarcia900200@gmail.com>
2025-02-19 20:13:11 -0600

    Add files via upload

---
+++ NET MAUI ejercicios/PersonRepository.cs
@@ -0,0 +1,64 @@
+﻿using People.Models;
+using SQLite;
+
+
+namespace People;
+
+public class PersonRepository
+{
+    string _dbPath;
+
+    public string StatusMessage { get; set; }
+
+    // TODO: Add variable for the SQLite connection
+
+    private SQLiteConnection conn;
+    private void Init()
+    {
+
+        if (conn != null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
+    }
+
+
+    public PersonRepository(string dbPath)
+    {
+        _dbPath = dbPath;
+    }
+
+    public void AddNewPerson(string name)
+    {
+        int result = 0;
+        
+        {
+            // enter this line
+            Init();
+
+            // basic validation to ensure a name was entered
+            if (string.IsNullOrEmpty(name))
+                throw new Exception("Valid name required");
+
+            // enter this line
+            result = conn.Insert(new Person { Name = name });
+        }
+        
+     }
+    public List<Person> GetAllPeople()
+    {
+        try
+        {
+            Init();
+            return conn.Table<Person>().ToList();
+        }
+        catch (Exception ex)
+        {
+            StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+        }
+
+        return new List<Person>();
+
+    } 
+}
